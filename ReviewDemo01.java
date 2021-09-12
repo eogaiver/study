39c0c95c4b71c85c56b0b6cb94adfbb1dc6b5700
eogaiver <eogaiver@hanmail.net>
2021-09-12 12:20:45 +0900

    demo01

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -6,16 +6,14 @@ public class ReviewDemo01 {
         int number = sc.nextInt();
         boolean isPrime = true;
 
-        for (int k=2; k<number; k++){
+        int k=2;
+        while ( k<number){
             if(number % k == 0){
                 isPrime = false;
                 break;
             }
+            k++;
         }
-
-        if(isPrime)
-            System.out.println(number + "은(는) 소수!");
-        else
-            System.out.println(number + "은(는) 소수가 아닙니다");
+        System.out.println(number + (isPrime? "은(는) 소수!" : "은(는) 소수가 아닙니다"));
     }
 }
